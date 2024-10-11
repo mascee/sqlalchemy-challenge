e3c1058920483253df387f792c274aa45c348d1f
Olga Petrova <mascee@gmail.com>
2024-10-11 16:58:37 -0700

    Added some routes to app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,10 +1,38 @@
 # Import the dependencies.
 
+from flask import Flask, jsonify, render_template
 
 
 #################################################
 # Database Setup
 #################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    results = session.query(Measurement.date, Measurement.prcp).all()
+    precipitation_data = {date: prcp for date, prcp in results}
+    return jsonify(precipitation_data)
+
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Climate app API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs"
+        f"/api/v1.0/<start>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
 
 # reflect an existing database into a new model
